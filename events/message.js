41af5bb64149d89eaa095fda892076a9b253e077
nerdpirado <73947102+nerdpirado@users.noreply.github.com>
2021-07-14 19:50:26 -0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -60,8 +60,8 @@ module.exports = async (client, message) => {
     )
       return client.sendError(
         message.channel,
-        "Missing Permissions!" + GuildDB.DJ
-          ? " You need the `DJ` role to access this command."
+        "Permissões ausentes!" + GuildDB.DJ
+          ? " Você precisa da função `DJ` para acessar este comando."
           : ""
       );
     cmd.run(client, message, args, { GuildDB });
